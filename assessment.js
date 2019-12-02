d877d76bbe73a8646222e518e9678e37c7deeb2c
BigBBaker <abobkerelaghel@gmail.com>
2019-12-02 09:55:19 +0100

    2/12

--- assessment.js
+++ assessment.js
@@ -28,7 +28,7 @@ function daBears() {
                 var sleepy = "Very yes";
             }
         }
-    }
+    } 
 
     function goldilocks() {
         var feeling = "Hungry";
@@ -43,12 +43,12 @@ function daBears() {
 // Which function(s) access the "chair" variable and get "Too Big!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale1 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale1 = ["papaBear", "mamaBear", "daBears"];
 
 // Which function(s) access the "feeling" variable and get "Hungry"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale2 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale2 = ["daBears",  "goldilocks"];
 
 // Which function(s) access the "porridge" variable and get "Too Cold!"
 // (Delete wrong answers, leave correct ones)
@@ -84,6 +84,23 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // CODE HERE...
 
 
+var Vehicle = function(){
+	this.gasRemaining = 100;
+
+}
+
+Vehicle.prototype.drive = Function(){
+ this.gasRemaining-=25;
+}
+
+var charger = new Vehicle();
+var mustang = new Vehicle();
+
+charger.drive();
+mustang.drive();
+mustang.drive();
+
+
 
 
 
@@ -104,10 +121,19 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // Your method may be passed punctuation, numbers or other non-letter characters
 // and should neither modify them nor break when encountering them.
 
-
-
-
 // CODE HERE...
+
+String.prototype.grammarPolice = function(){
+	var temp = this;
+	temp = temp.toLowerCase();
+	temp[0] = temp[0].toUpperCase();
+	for (var i = 0; i < temp.length; i++) {
+	if( temp.charAt(i) === ' '){
+		temp[i+1].toUpperCase();
+	}
+}
+return temp;
+}
 
 
 
@@ -126,6 +152,17 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // In all other cases, return "Different values"
 
 // CODE HERE...
+
+function valueType(x , y){
+
+	if(x === y)
+		return 'Exactly the same';
+    else if (x == y)
+    	return 'Same value, different types';
+    else 
+    	return 'Different values'
+}
+
 
 // *************
 // * PROBLEM 5 *
@@ -153,6 +190,12 @@ function large() {
 }
   // CODE HERE...
 
+var boundToElephant = large.bind(elephant);
+boundToElephant();
+
+
+
+
 // *************
 // * PROBLEM 6 *
 // *************
@@ -164,6 +207,11 @@ function large() {
 // and return the bound function.
 
 // CODE HERE...
+
+function deathStar(capacity,crew){
+	return capacity.bind(crew);
+}
+
 
 
 // *************
@@ -178,3 +226,11 @@ function large() {
 // The closure function will return the combined value of assets and liabilities.
 
 // CODE HERE...
+
+function accountingOffice(assets){
+	function liabilitiesFunction(liabilities){
+		return assets + liabilities;
+	}
+	return liabilitiesFunction;
+}
+accountingOffice(4)(6); // 10
